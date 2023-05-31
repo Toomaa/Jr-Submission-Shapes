640dee44b4c16225fcee3e98f12f5b78b36287dc
Tomaž Vovk <tomaz.vovk@gmail.com>
2023-05-31 15:05:47 +0200

    Wrapping up

--- Assets/Scripts/UI/ShapeSelection.cs
+++ Assets/Scripts/UI/ShapeSelection.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -17,14 +18,23 @@ public class ShapeSelection : MonoBehaviour
         _nameText.gameObject.SetActive(false);
     }
 
-    private void OnMouseDown()
+    private void OnMouseUpAsButton()
     {
+        StartCoroutine(ShowPanel());
+    }
+
+    private IEnumerator ShowPanel()
+    {
+        yield return new WaitForEndOfFrame();
+
         // deactivate other panels
         foreach (GameObject o in GameObject.FindGameObjectsWithTag("Panel"))
         {
             if (_panel.gameObject.GetInstanceID() == o.GetInstanceID()) continue;
             o.SetActive(false);
         }
+
+        yield return new WaitForEndOfFrame();
 
         _panel.gameObject.SetActive(true);
     }
